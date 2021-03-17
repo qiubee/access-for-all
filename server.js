2a362e4fc5afbce9087c10fde697e0f524bf9873
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-17 16:13:12 +0100

    add server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const hbs = require("express-handlebars");
+const router = require("./routes/router");
+
+require("dotenv").config();
+
+// server
+const app = express();
+const port = process.env.PORT || 8000;
+
+// set handlebars as templating engine
+app.set("view engine", "hbs")
+	.engine( "hbs", hbs({ 
+		extname: "hbs", 
+		defaultLayout: "default", 
+		layoutsDir: __dirname + "/views/layouts/",
+		partialsDir: __dirname + "/views/partials/"
+	}));
+
+app.use(express.static("public")) // use public folder for static files
+	.use(express.urlencoded({extended: true})) // get data from http body
+	.use("/", router) // routing
+	.disable("x-powered-by")
+	.listen(port, function () {
+	console.log(`Listening on localhost:${port}`);
+});
