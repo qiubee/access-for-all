bb2feaca9e01b505a1078f11b12767b20b0c0331
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-18 20:05:47 +0100

    add multiple get routes

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,20 @@ const router = require("express").Router();
 
 // controllers
 const index = require("./controllers/index");
+const { poll, searchPolls } = require("./controllers/poll");
+const { creators, createCreator, myPolls } = require("./controllers/creator");
+const { createUser } = require("./controllers/user");
+const { creatorLogin, userLogin } = require("./controllers/login");
 
-router.get("/", index);
+router.get("/", index)
+	.get("/zoek-poll", searchPolls)
+	.get("/creators", creators)
+	.get("/login", userLogin)
+	.get("/profiel-aanmaken", createUser)
+	.get("/creator-login", creatorLogin)
+	.get("/creator-profiel-aanmaken", createCreator)
+	.get("/mijn-polls", myPolls)
+	.get("/poll/:pollID", poll)
+	// .post("/login", authentication);
 
 module.exports = router;
