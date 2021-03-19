6e1cff35116583df994acf3adf622e99058b0f42
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-19 21:19:35 +0100

    remove authentication function

--- routes/controllers/login.js
+++ routes/controllers/login.js
@@ -10,14 +10,6 @@ function userLogin(req, res) {
 	});
 }
 
-function authentication(req, res) {
-	const loginCredentials = req.body;
-	// 1. authenticate user
-	// 2. set authorization header
-	// 3. if user is creator redirect to creator view else to user view
-	res.redirect("/");
-}
 
 exports.creatorLogin = creatorLogin;
 exports.userLogin = userLogin;
-exports.authentication = authentication;
