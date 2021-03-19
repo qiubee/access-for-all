49946084aba921c88ce1e24f15f3ab53d4249e0d
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-19 21:17:37 +0100

    add auth controller & update get routes

--- routes/router.js
+++ routes/router.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 
 // controllers
 const index = require("./controllers/index");
+const { authenticate, authorized } = require("./controllers/authentication");
 const { poll, searchPolls } = require("./controllers/poll");
-const { creators, createCreator, myPolls } = require("./controllers/creator");
-const { createUser } = require("./controllers/user");
+const { creators, createCreator, creatorProfile, myPolls } = require("./controllers/creator");
+const { createUser, userProfile } = require("./controllers/user");
 const { creatorLogin, userLogin } = require("./controllers/login");
 
 router.get("/", index)
@@ -14,8 +15,10 @@ router.get("/", index)
 	.get("/profiel-aanmaken", createUser)
 	.get("/creator-login", creatorLogin)
 	.get("/creator-profiel-aanmaken", createCreator)
-	.get("/mijn-polls", myPolls)
-	.get("/poll/:pollID", poll)
-	// .post("/login", authentication);
+	.get("/c/:creatorname([a-zA-Z]{2,10})", authorized, creatorProfile)
+	.get("/c/:creatorname([a-zA-Z]{2,10})/mijn-polls", authorized, myPolls)
+	.get("/u/:username([a-zA-Z]{3,16})/", authorized, userProfile)
+	.get("/poll/:pollID(\d{8})", poll)
+	.post("/login", authenticate);
 
 module.exports = router;
