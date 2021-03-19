03f09b2178c66d85eea5acf0b4fef9293a51f88c
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-19 21:19:53 +0100

    add userProfile function

--- routes/controllers/user.js
+++ routes/controllers/user.js
@@ -1,7 +1,18 @@
+const { readFromFile } = require("../../modules/file");
+
 function createUser(req, res) {
 	res.render("createUser", {
 		title: "Maak profiel · Polly"
 	});
 }
 
+function userProfile(req, res) {
+	const username = req.params.username || "Poller";
+	res.render("userPage", {
+		title: `${username} · Polly`,
+		username: username
+	});
+}
+
 exports.createUser = createUser;
+exports.userProfile = userProfile;
