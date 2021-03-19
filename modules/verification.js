2045df68ccb129add2cf8dd67fe51fe7a175b6bb
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-19 21:16:22 +0100

    add verification module

---
+++ modules/verification.js
@@ -0,0 +1,34 @@
+const { readFromFile } = require("./file");
+
+function verifyUser(username, password, type) {
+	const users = JSON.parse(readFromFile("data/users.json"));
+	
+	if (type === "creator") {
+		const creator = lookupUser(users.creator, username, password);
+		if (creator) {
+			return creator;
+		} else {
+			return false;
+		}
+	} else {
+		const user = lookupUser(users.user, username, password);
+		if (user) {
+			return user;
+		} else {
+			const creator = lookupUser(users.creator, username, password);
+			if (creator) {
+				return creator;
+			} else {
+				return false;
+			}
+		}
+	}
+}
+
+function lookupUser(users, username, password) {
+	return users.find(function (user) {
+		return user.username === username && user.password === password; 
+	});
+}
+
+exports.verifyUser = verifyUser;
