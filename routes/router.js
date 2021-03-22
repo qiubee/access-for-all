b46662f768d6d7b071a47db28f8c16b7e0d8cd82
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-22 19:56:32 +0100

    add get & post routes, import handlers

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 // controllers
 const index = require("./controllers/index");
 const { authenticate, authorized } = require("./controllers/authentication");
-const { poll, searchPolls } = require("./controllers/poll");
+const { poll, searchPolls, pollCreator, pollEditor, addPoll, editPoll } = require("./controllers/poll");
 const { creators, createCreator, creatorOverview } = require("./controllers/creator");
 const { createUser, userOverview } = require("./controllers/user");
 const { creatorLogin, userLogin } = require("./controllers/login");
@@ -17,7 +17,10 @@ router.get("/", index)
 	.get("/creator-profiel-aanmaken", createCreator)
 	.get("/c/:creatorname([a-zA-Z]{2,10})", authorized, creatorOverview)
 	.get("/u/:username([a-zA-Z]{3,16})", authorized, userOverview)
+	.get("/c/:creatorname([a-zA-Z]{2,10})/maak-poll", authorized, pollCreator)
+	.get("/c/:creatorname([a-zA-Z]{2,10})/maak-poll", authorized, pollEditor)
 	.get("/poll/:pollID(\d{8})", poll)
-	.post("/login", authenticate);
+	.post("/login", authenticate)
+	.post("/maak-poll", addPoll);
 
 module.exports = router;
