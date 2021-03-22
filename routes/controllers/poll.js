5945a08d341bb38001efd268b03b697c33b8c48e
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-22 19:56:09 +0100

    add poll functions & import database module

--- routes/controllers/poll.js
+++ routes/controllers/poll.js
@@ -1,3 +1,5 @@
+const db = require("./../../modules/database");
+
 function poll(req, res) {
 	const params = req.params;
 	const pollTitle = "Poll";
@@ -12,5 +14,37 @@ function searchPolls(req, res) {
 	});
 }
 
+function pollCreator(req, res) {
+	const creatorname = req.params.creatorname || "Creator";
+	res.render("createPoll", {
+		title: `Nieuwe poll maken · ${creatorname} | Polly`,
+		creatorname: creatorname
+	});
+}
+
+function pollEditor(req, res) {
+	const creatorname = req.params.creatorname || "Creator";
+	res.render("editPoll", {
+		title: `Poll bewerken · ${creatorname} | Polly`
+	});
+}
+
+function addPoll(req, res) {
+	const pollData = req.body;
+	console.log(pollData);
+	db.add("polls", pollData);
+	// 1. get poll data
+	// 2. add poll to poll.json
+	// 3. forward to edit poll page
+}
+
+function editPoll(req, res) {
+	
+}
+
 exports.searchPolls = searchPolls;
 exports.poll = poll;
+exports.pollCreator = pollCreator;
+exports.pollEditor = pollEditor;
+exports.addPoll = addPoll;
+exports.editPoll = editPoll;
