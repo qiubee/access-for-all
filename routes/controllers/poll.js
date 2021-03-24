8230284adbf4f6eae64b277afc5a87813417a623
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-24 18:48:42 +0100

    update addPoll function & add id generate function

--- routes/controllers/poll.js
+++ routes/controllers/poll.js
@@ -1,4 +1,6 @@
 const db = require("./../../modules/database");
+const file = require("./../../modules/file");
+const { currentDateTime } = require("../../modules/date");
 
 function poll(req, res) {
 	const params = req.params;
@@ -25,7 +27,7 @@ function pollCreator(req, res) {
 
 function pollEditor(req, res) {
 	const creatorname = req.params.creatorname;
-	const pollID = req.params.pollID;
+	const pollId = req.params.pollId;
 	// 1. find poll
 	// 2. add poll name to variable
 	const pollname = "titel"; // temporary
@@ -50,7 +52,7 @@ function questionCreator(req, res) {
 	}
 	res.render("createQuestion", {
 		title: `Vraag toevoegen · ${data.creatorname} | Polly`,
-		pollID: data.pollId,
+		pollId: data.pollId,
 		creatorname: data.creatorname,
 		optionCount: optionCount + 1,
 		nextOption: optionCount > 4 ? false : true,
@@ -67,16 +69,39 @@ function questionEditor(req, res) {
 
 // data processing
 function addPoll(req, res) {
-	const pollData = req.body;
-	const creatorname = req.body.creatorname;
-	console.log(pollData);
-	// db.add("polls", pollData);
-	// 1. get poll data
-	// 2. add poll to poll.json
-	// 3. forward to edit poll page
-	// 4. get pollID
-	const pollID = "00000000";
-	res.redirect(`/c/${creatorname}/poll/${pollID}/edit`);
+	const newPoll = req.body;
+	const creatorname = newPoll.creatorname;
+	const pollId = generatePollID();
+
+	// validate
+	newPoll.title = newPoll.title.replace(/[\^*()_+|~`.{}"\;\[\]'<>\/]/g,"").trim();
+
+	// add properties
+	newPoll.pollId = pollId;
+	newPoll.active = false;
+	newPoll.creator = newPoll.creatorname;
+	newPoll.questionCount = 0;
+	newPoll.questions = [];
+	newPoll.totalvotes = 0;
+	newPoll.timeStandard = {
+		checked: true,
+		timeInSeconds: newPoll.timePerQuestion
+	};
+	newPoll.modified = `${currentDateTime()}`;
+	newPoll.created = `${currentDateTime()}`;
+
+	delete newPoll.timePerQuestion;
+	delete newPoll.creatorname;
+
+
+	// read polls.json & add new poll
+	const allPolls = file.readJSON("data/polls.json");
+	allPolls.push(newPoll);
+
+	// add to polls.json
+	db.add("polls", allPolls);
+	// redirect to edit poll
+	res.redirect(`/c/${creatorname}/poll/${pollId}/edit`);
 }
 
 function addQuestion(req, res) {
@@ -90,6 +115,27 @@ function editPoll(req, res) {
 
 function editQuestion(req, res) {
 	
+}
+
+function generatePollID() {
+	const madePolls = file.readJSON("data/polls.json");
+	
+	const pollIds = madePolls.map(function (poll) {
+		return poll.pollId;
+	});
+	while (true) {
+		// generate id
+		const iteration = Math.floor(Math.random() * (10 - 8) + 8);
+		let id = "";
+		for (let i = 0; i < iteration; i++) {
+			id += Math.floor(Math.random() * Math.floor(10));
+		}
+		// check for duplicate
+		const duplicate = pollIds.includes(id);
+		if (!duplicate) {
+			return id;
+		}
+	}
 }
 
 exports.search = searchPolls;
