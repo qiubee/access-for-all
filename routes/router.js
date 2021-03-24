b0b40585724d4640b48dd347dd4afe413c4adc67
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-24 15:27:47 +0100

    update imports & add post routes

--- routes/router.js
+++ routes/router.js
@@ -3,27 +3,29 @@ const router = require("express").Router();
 // controllers
 const index = require("./controllers/index");
 const { authenticate, authorized } = require("./controllers/authentication");
-const { poll, searchPolls, pollCreator, pollEditor, questionCreator, questionEditor, addPoll, editPoll, addQuestion } = require("./controllers/poll");
-const { creators, createCreator, creatorOverview } = require("./controllers/creator");
-const { createUser, userOverview } = require("./controllers/user");
-const { creatorLogin, userLogin } = require("./controllers/login");
+const poll = require("./controllers/poll");
+const creator = require("./controllers/creator");
+const user = require("./controllers/user");
+const login = require("./controllers/login");
 
 router.get("/", index)
-	.get("/zoek-poll", searchPolls)
-	.get("/creators", creators)
-	.get("/login", userLogin)
-	.get("/profiel-aanmaken", createUser)
-	.get("/creator-login", creatorLogin)
-	.get("/creator-profiel-aanmaken", createCreator)
-	.get("/c/:creatorname([a-zA-Z]{2,10})", authorized, creatorOverview)
-	.get("/u/:username([a-zA-Z]{3,16})", authorized, userOverview)
-	.get("/c/:creatorname([a-zA-Z]{2,10})/maak-poll", authorized, pollCreator)
-	.get("/c/:creatorname([a-zA-Z]{2,10})/poll/:pollId([0-9]{8,})/edit", authorized, pollEditor)
-	.get("/c/:creatorname([a-zA-Z]{2,10})/poll/:pollId([0-9]{8,})/vraag-toevoegen:options?", authorized, questionCreator)
-	.get("/c/:creatorname([a-zA-Z]{2,10})/poll/:pollId([0-9]{8,})/vraag-:question([0-9]{1,2})", authorized, questionEditor)
-	.get("/poll/:pollId([0-9]{8,})", poll)
+	.get("/zoek-poll", poll.search)
+	.get("/creators", creator.infoPage)
+	.get("/login", login.user)
+	.get("/profiel-aanmaken", user.create)
+	.get("/creator-login", login.creator)
+	.get("/creator-profiel-aanmaken", creator.create)
+	.get("/c/:creatorname([a-zA-Z]{2,10})", authorized, creator.overview)
+	.get("/u/:username([a-zA-Z]{3,16})", authorized, user.overview)
+	.get("/c/:creatorname([a-zA-Z]{2,10})/maak-poll", authorized, poll.creator)
+	.get("/c/:creatorname([a-zA-Z]{2,10})/poll/:pollId([0-9]{8,})/edit", authorized, poll.editor)
+	.get("/c/:creatorname([a-zA-Z]{2,10})/poll/:pollId([0-9]{8,})/vraag-toevoegen:options?", authorized, poll.createQuestion)
+	.get("/c/:creatorname([a-zA-Z]{2,10})/poll/:pollId([0-9]{8,})/vraag-:question([0-9]{1,2})", authorized, poll.editQuestion)
+	.get("/poll/:pollId([0-9]{8,})", poll.poll)
 	.post("/login", authenticate)
-	.post("/maak-poll", addPoll)
-	.post("/vraag-toevoegen", addQuestion);
+	.post("/maak-poll", poll.add)
+	.post("/vraag-toevoegen", poll.addQuestion)
+	.post("/bewerk-poll", poll.edit)
+	.post("/bewerk-vraag", poll.editQuestion);
 
 module.exports = router;
