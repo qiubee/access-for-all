e3bf4efad041ef524470ff4b40d7675e90be4f09
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-24 13:12:17 +0100

    fix displaying single digit number

--- modules/date.js
+++ modules/date.js
@@ -1,9 +1,9 @@
 function currentDateTime() {
     const date = new Date(new Date().setDate(new Date().getDate()));
-    const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
-    const month = "0" + (date.getMonth() + 1);
     const year = date.getFullYear();
-    const hours = date.getHours();
+    const month = (date.getMonth() + 1) < 10 ? "0" + (date.getMonth() + 1) : (date.getMonth() + 1);
+    const day = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+    const hours = date.getHours() < 10 ? "0" + date.getHours() : date.getHours();
     const minutes = date.getMinutes();
     const seconds = date.getSeconds() < 10 ? "0" + date.getSeconds() : date.getSeconds();
     return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
