a1b545bbe8aa825404ae56793ed66e10f5cdd8dc
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-24 18:51:50 +0100

    update to stringify and write data to JSON file

--- modules/database.js
+++ modules/database.js
@@ -1,8 +1,8 @@
 const file = require("./file");
 
 function addToDatabase(name, data) {
-	const polls = file.readFromFile("data/" + name + ".json");
-	console.log(polls);
+	JSON.stringify(file.writeToJSON("data/" + name + ".json", data));
+	console.log("Data added to: \"" + name + ".json\"");
 }
 
 exports.add = addToDatabase;
