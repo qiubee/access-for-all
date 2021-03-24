fb6bdcf4764298216c63826ce6a3d1473b856314
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-24 14:26:23 +0100

    add question routes & handlers

--- routes/router.js
+++ routes/router.js
@@ -3,7 +3,7 @@ const router = require("express").Router();
 // controllers
 const index = require("./controllers/index");
 const { authenticate, authorized } = require("./controllers/authentication");
-const { poll, searchPolls, pollCreator, pollEditor, addPoll, editPoll } = require("./controllers/poll");
+const { poll, searchPolls, pollCreator, pollEditor, questionCreator, questionEditor, addPoll, editPoll, addQuestion } = require("./controllers/poll");
 const { creators, createCreator, creatorOverview } = require("./controllers/creator");
 const { createUser, userOverview } = require("./controllers/user");
 const { creatorLogin, userLogin } = require("./controllers/login");
@@ -19,8 +19,11 @@ router.get("/", index)
 	.get("/u/:username([a-zA-Z]{3,16})", authorized, userOverview)
 	.get("/c/:creatorname([a-zA-Z]{2,10})/maak-poll", authorized, pollCreator)
 	.get("/c/:creatorname([a-zA-Z]{2,10})/poll/:pollId([0-9]{8,})/edit", authorized, pollEditor)
+	.get("/c/:creatorname([a-zA-Z]{2,10})/poll/:pollId([0-9]{8,})/vraag-toevoegen:options?", authorized, questionCreator)
+	.get("/c/:creatorname([a-zA-Z]{2,10})/poll/:pollId([0-9]{8,})/vraag-:question([0-9]{1,2})", authorized, questionEditor)
 	.get("/poll/:pollId([0-9]{8,})", poll)
 	.post("/login", authenticate)
-	.post("/maak-poll", addPoll);
+	.post("/maak-poll", addPoll)
+	.post("/vraag-toevoegen", addQuestion);
 
 module.exports = router;
