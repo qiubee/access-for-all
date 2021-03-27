547513e35dfe21e7de123f760b72fdee17d42840
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-27 23:26:12 +0100

    add env variable & add readFromDatabase function

--- modules/database.js
+++ modules/database.js
@@ -2,8 +2,14 @@ const file = require("./file");
 const { currentDateTime } = require("./date");
 
 function addToDatabase(name, data) {
-	JSON.stringify(file.writeToJSON("data/" + name + ".json", data));
+	JSON.stringify(file.writeToJSON(`${process.env.DB_FOLDER}/` + name + ".json", data));
 	console.log(currentDateTime() + ": Data added to: \"" + name + ".json\"");
 }
 
+function readFromDatabase(name) {
+	console.log(currentDateTime() + ": Data loaded from: \"" + name + ".json\"");
+	return file.readJSONFile(`${process.env.DB_FOLDER}/` + name + ".json");
+}
+
 exports.add = addToDatabase;
+exports.read = readFromDatabase;
