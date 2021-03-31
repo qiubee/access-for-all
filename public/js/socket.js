8a9a653e8d1a09166f9fac01b229164d4a394df0
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-31 21:20:23 +0200

    remove submit & update button & add data standard

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,14 +1,29 @@
 if (window.WebSocket) {
+	const results = document.querySelectorAll("main.poll div span.results");
+
+	// remove update & vote button
+	document.querySelector("main.poll article section a:last-of-type").remove();
+	const votebutton = document.querySelector("form[action=\"/vote\"] input[type=\"submit\"]").remove();
+
 	const host = location.origin.replace(/^http/, "ws");
 	const socket = new WebSocket(host);
 	socket.addEventListener("open", function () {
-		socket.send("connection established");
+		const newConnectionMessage = {
+			type: "JSON",
+			message: "connection established"
+		};
+
+		const data = JSON.stringify(newConnectionMessage);
+		socket.send(data);
 	});
 
 	socket.addEventListener("message", function (message) {
-		console.log(message.data);
+		const data = JSON.parse(message.data);
+		console.log(data);
+
+		socket.send(JSON.stringify({type: "JSON", data: {
+		}}));
 	});
 } else if (window.fetch) {
 
-} else {
 }
