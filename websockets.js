cd204dffaca156af5a3fe6e71a27dbee95b1b2a1
qiubee <45637654+qiubee@users.noreply.github.com>
2021-03-31 21:18:39 +0200

    add errorMessage function & save socket count

--- websockets.js
+++ websockets.js
@@ -1,12 +1,47 @@
+const { currentDateTime } = require("./modules/date");
 const WebSocket = require("ws");
 
 module.exports = function (server) {
 	const wss = new WebSocket.Server({ server });
 
-	wss.on("connection", function connection(socket) {
+	let clients = 0;
+	wss.on("connection", function connection(socket, req) {
+		clients++;
+		console.log(currentDateTime() + `: New socket client (total: ${clients})`);
+
 		socket.on("message", function incoming(message) {
-			console.log("received: " + message);
-			socket.send("message received");
+			console.log(currentDateTime() + ": New message received");
+			let output = {
+				type: "JSON"
+			};
+
+			try {
+				input = JSON.parse(message);
+			} catch (err) {
+				console.log(currentDateTime() + ": Data not of JSON type");
+				sendError(socket, "Not of JSON type");
+				return;
+			}
+
+			
+
+			// const newMessage = JSON.stringify(output);
+			// socket.send(newMessage);
 		});
+
+		socket.on("close", function close() {
+			clients--;
+			console.log(currentDateTime() + ": Socket client disconnected");
+		});
+
+		
 	});
 };
+
+function sendError(socket, errorMessage) {
+	const errMsg = {
+		type: "ERROR",
+		message: errorMessage
+	};
+	socket.send(JSON.stringify(errMsg));
+}
