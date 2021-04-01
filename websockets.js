a74accea7427123c1640f18acd42829718152263
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-01 22:02:33 +0200

    add new event, watch poll changes & push data

--- websockets.js
+++ websockets.js
@@ -1,5 +1,11 @@
 const { currentDateTime } = require("./modules/date");
 const WebSocket = require("ws");
+const { watch } = require("fs");
+const db = require("./modules/database");
+const { eventNames } = require("process");
+require("dotenv").config();
+
+const FOLDER = process.env.DB_FOLDER || "data";
 
 module.exports = function (server) {
 	const wss = new WebSocket.Server({ server });
@@ -10,20 +16,41 @@ module.exports = function (server) {
 		console.log(currentDateTime() + `: New socket client (total: ${clients})`);
 
 		socket.on("message", function incoming(message) {
-			console.log(currentDateTime() + ": New message received");
-			let output = {
-				type: "JSON"
-			};
-
 			try {
 				input = JSON.parse(message);
 			} catch (err) {
-				console.log(currentDateTime() + ": Data not of JSON type");
-				sendError(socket, "Not of JSON type");
+				console.log(currentDateTime() + ": Data not of type JSON");
+				sendError(socket, "Not of type JSON");
 				return;
 			}
 
-			
+			if (input.event === "results") {
+				const data = input.data;
+				const pollId = data.pollId;
+				const questionIndex = data.questionIndex;
+				logMessage(input.message);
+
+				let poll = db.read("polls").find(function (poll) {
+					return poll.pollId === pollId;
+				});
+				watch(FOLDER + "/polls.json", function (event) {
+					if (event === "change") {
+						const updatedPoll = db.read("polls").find(function (poll) {
+							return poll.pollId === pollId;
+						});
+
+						if (poll !== updatedPoll) {
+							const newResults = updatedPoll.questions.find(function (question) {
+								return question.index === questionIndex;
+							}).options;
+
+							poll = updatedPoll;
+
+							socket.send(createPackage("results", newResults, "new poll results"));
+						}
+					}
+				});
+			}
 
 			// const newMessage = JSON.stringify(output);
 			// socket.send(newMessage);
@@ -31,7 +58,7 @@ module.exports = function (server) {
 
 		socket.on("close", function close() {
 			clients--;
-			console.log(currentDateTime() + ": Socket client disconnected");
+			console.log(currentDateTime() + `: Client disconnected (total: ${clients})`);
 		});
 
 		
@@ -44,4 +71,18 @@ function sendError(socket, errorMessage) {
 		message: errorMessage
 	};
 	socket.send(JSON.stringify(errMsg));
+}
+
+function createPackage(eventname, data, message = "") {
+	let package = {
+		type: "JSON",
+		event: eventname,
+		data: data
+	};
+	if (message) { package.message = message; }
+	return JSON.stringify(package);
+}
+
+function logMessage(message) {
+	return console.log(currentDateTime() + ": Message: " + message);
 }
