45fbd5251f87a15444a3fe86ce743202bdb5acb1
qiubee <45637654+qiubee@users.noreply.github.com>
2021-04-01 22:03:48 +0200

    retrieve & update results

--- public/js/socket.js
+++ public/js/socket.js
@@ -1,16 +1,40 @@
 if (window.WebSocket) {
-	const results = document.querySelectorAll("main.poll div span.results");
-
-	// remove update & vote button
-	document.querySelector("main.poll article section a:last-of-type").remove();
-	const votebutton = document.querySelector("form[action=\"/vote\"] input[type=\"submit\"]").remove();
-
+	// get websocket hostname
 	const host = location.origin.replace(/^http/, "ws");
+
+	// result elements
+	const results = document.querySelectorAll("main.poll span.results");
+
+	// remove update
+	document.querySelector("main.poll article section a[href^=\"/poll\"]").remove();
+
+	// add click event to options
+	const options = document.querySelectorAll("form[action=\"/vote\"] fieldset div");
+	options.forEach(function (option) {
+		option.addEventListener("click", function (event) {
+			console.log("option pressed");
+		});
+	});
+
+	// create socket & add listeneres
 	const socket = new WebSocket(host);
 	socket.addEventListener("open", function () {
+		const pollId = window.location.pathname.match(/[0-9]{8,12}/g)[0];
+		let questionIndex;
+		if (document.querySelector("input[name=\"index\"]")) {
+			questionIndex = parseInt(document.querySelector("input[name=\"index\"]").value);
+		} else if (document.querySelector("p[data-index]")) {
+			questionIndex = parseInt(document.querySelector("p[data-index]").getAttribute("data-index"));
+		}		
+
 		const newConnectionMessage = {
 			type: "JSON",
-			message: "connection established"
+			event: "results",
+			message: "connection established",
+			data: {
+				pollId: pollId,
+				questionIndex: questionIndex
+			}
 		};
 
 		const data = JSON.stringify(newConnectionMessage);
@@ -18,12 +42,30 @@ if (window.WebSocket) {
 	});
 
 	socket.addEventListener("message", function (message) {
-		const data = JSON.parse(message.data);
-		console.log(data);
+		let data;
+		try {
+			data = JSON.parse(message.data);
+		} catch (err) {
+			console.log("Data not of type JSON");
+			return;
+		}
 
-		socket.send(JSON.stringify({type: "JSON", data: {
-		}}));
+		if (data.event === "results") {
+			// update results
+			const newResults = data.data;
+			results.forEach(function (result, index) {
+				result.textContent = newResults[index].votes;
+			});
+
+			// update total
+			const total = newResults.reduce(function (acc, next) {
+				return acc + next.votes;
+			}, 0);
+			document.querySelector("main.poll span.total").textContent = total;
+		}
 	});
-} else if (window.fetch) {
+}
 
+function sendData(socket, data) {
+	socket.send(JSON.stringify({ type: "JSON", data: data }));
 }
